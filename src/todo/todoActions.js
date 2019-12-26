3e863221de8b0d2e15d7eb182609a5aef23b1612
joaoh <joao_camargo10@hotmail.com>
2019-12-26 19:55:40 -0300

    Mark as done/pending

--- src/todo/todoActions.js
+++ src/todo/todoActions.js
@@ -22,3 +22,17 @@ export const add = (description) => {
             .then(resp => dispatch(search()))
     }
 }
+
+export const markAsDone = (todo) => {
+    return dispatch => {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: true })
+            .then(resp => dispatch(search()))
+    }
+}
+
+export const markAsPending = todo => {
+    return dispatch => {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: false })
+            .then(resp => dispatch(search()))
+    }
+}
--- src/todo/todoList.jsx
+++ src/todo/todoList.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
 import IconButton from '../template/iconButton'
+import { markAsDone, markAsPending } from './todoActions'
 
 const TodoList = props => {
 
@@ -13,9 +15,9 @@ const TodoList = props => {
                 <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
                 <td>
                     <IconButton style='success' icon='check' hide={todo.done}
-                        onClick={() => props.handleMarkAsDone(todo)} />
+                        onClick={() => props.markAsDone(todo)} />
                     <IconButton style='warning' icon='undo' hide={!todo.done}
-                        onClick={() => props.handleMarkAsPending(todo)} />
+                        onClick={() => props.markAsPending(todo)} />
                     <IconButton style='danger' icon='trash-o' hide={!todo.done}
                         onClick={() => props.handleRemove(todo)} />
                 </td>
@@ -39,4 +41,5 @@ const TodoList = props => {
 }
 
 const mapStateToProps = state => ({ list: state.todo.list })
-export default connect(mapStateToProps)(TodoList)
+const mapDispatchToProps = dispatch => bindActionCreators({ markAsDone, markAsPending }, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
